72fc4f66deba8cdc712c586e340c488f0b0dd933
Wael <wael.tarifi123@gmail.com>
2020-01-17 15:43:17 -0500

    test

--- TP1/TP1-H20/TP1-H20-Unix/src/Film.cpp
+++ TP1/TP1-H20/TP1-H20-Unix/src/Film.cpp
@@ -45,7 +45,6 @@ namespace
         return NOMS_PAYS[index];
     }
 } // namespace
-// git 
 // TODO: Constructeur par paramètre en utilisant la liste d'initialisation
 // Utiliser CAPACITE_PAYS_INITIALE pour la taille initiale de paysRestreints_
 // (allocation dynamique!)
