15d6722e573b2e1d880e6726021dcc6bf5c1ab65
Jamesley Joseph <jamesleyjoseph5@gmail.com>
2020-02-29 15:57:49 -0500

    third commit

--- TP3/src/Film.cpp
+++ TP3/src/Film.cpp
@@ -1,31 +1,41 @@
 // To do
 #include "Film.h"
-// To do
-Film::Film(Auteur* auteur)
+
+// constructeur par defaut de la classe film
+Film::Film(Auteur* auteur):
+Media(auteur, Media::TypeMedia::Film),
+duree_("unknow")
 {
 }
 
-// To do
+// constructeur par parametre de la classe film 
 Film::Film(const std::string& nom, unsigned int anneeDeSortie, Genre genre, Pays pays,
-           bool estRestreintParAge, Auteur* auteur, const std::string& duree)
-    // To do
+           bool estRestreintParAge, Auteur* auteur, const std::string& duree):
+Media(nom ,anneeDeSortie ,genre ,pays ,estRestreintParAge ,auteur, Media::TypeMedia::Film),
+duree_(duree)
 {
 }
 
 // To do
 std::ostream& Film::afficher(std::ostream& os) const
 {
-    // To do
+    os << duree_; 
+    Media::afficher(os); 
+    return os ; 
+
 }
 
 // To do
 std::istream& Film::lire(std::istream& is)
 {
-    // To do
+     is >> duree_;
+     Media::lire(is); 
+     return is; 
+
 }
 
 // To do
 std::unique_ptr<Media> Film::clone() const
 {
-    // To do
+ return std::make_unique<Media>(*this); 
 }
